905c939155814b2565b81dd3a7a5dd6cc541ee5b
Cezary Wojcik <cezarywojcik92@gmail.com>
2013-07-14 22:54:03 -0700

    prepared statements

--- includes/functions.php
+++ includes/functions.php
@@ -1,12 +1,49 @@
 <?php
 
+// get array of results from executed prepared statement
+function fetch($result) {
+    $array = array();
+    if($result instanceof mysqli_stmt)
+    {
+        $result->store_result();
+        $variables = array();
+        $data = array();
+        $meta = $result->result_metadata();
+        while($field = $meta->fetch_field())
+            $variables[] = &$data[$field->name];
+        call_user_func_array(array($result, 'bind_result'), $variables);
+        $i=0;
+        while($result->fetch())
+        {
+            $array[$i] = array();
+            foreach($data as $k=>$v)
+                $array[$i][$k] = $v;
+            $i++;
+        }
+    }
+    elseif($result instanceof mysqli_result)
+    {
+        while($row = $result->fetch_assoc())
+            $array[] = $row;
+    }
+
+    return $array;
+}
+
+// get contents of article
 function getArticle($id) {
     global $mysqli;
-    $queryString = "SELECT * FROM blog WHERE blogid=$id";
-    $query = $mysqli->query($queryString);
-    $row = $query->fetch_assoc();
-    $row['timecreated'] = date('F j, Y', strtotime($row['timecreated']));
-    return $row;
+    $result = array();
+    $queryString = "SELECT * FROM blog WHERE blogid=?";
+    if ($statement = $mysqli->prepare($queryString)) {
+        $statement->bind_param("i", $id);
+        $statement->execute();
+        $result = fetch($statement)[0];
+        $statement->close();
+        $result['timecreated'] = date('F j, Y',
+            strtotime($result['timecreated']));
+    }
+    return $result;
 }
 
 function getArticlePreview($id) {
@@ -19,8 +56,12 @@ function getArticlePreview($id) {
 
 function getMaxArticleId() {
     global $mysqli;
+    $result = array();
     $queryString = "SELECT MAX(blogid) FROM blog";
-    $query = $mysqli->query($queryString);
-    $row = $query->fetch_assoc();
-    return $row['MAX(blogid)'];
+    if ($statement = $mysqli->prepare($queryString)) {
+        $statement->execute();
+        $result = fetch($statement)[0];
+        $statement->close();
+    }
+    return $result['MAX(blogid)'];
 }
